28a6c2a6bc5fe9ae5a15a4169dbfb4ec06d7cbff
ytiK0 <eeeee@eee.eee>
2025-09-11 12:41:48 +0300

    Remove legacy animation engine.

--- src/lib/animationJson/constst.ts
+++
@@ -1,5 +0,0 @@
-export const MOVE_PARTS: MovePart[] = ["head", "leftArm", "leftLeg", "rightArm", "rightLeg", "torso"];
-
-export const MOVE_TRANSFORMS_DIR: MoveTransformDir[] = ["x", "y", "z", "pitch", "yaw", "roll", "bend", "axis"];
-
-export const ROTATION_TRANSFORMS_DIR: MoveTransformDir[] = ["pitch", "yaw", "roll"];
