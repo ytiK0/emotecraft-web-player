3b6a02476a5f97b47945e451beb4e75ca5582554
ytiK0 <eeeee@eee.eee>
2025-09-12 18:49:12 +0300

    Refactor emote handling by removing legacy code and adding new emotes.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import {Canvas} from "@react-three/fiber";
 import {Grid, OrbitControls} from "@react-three/drei";
 import {Color} from "three";
 import EmotePlayer from "@/EmotePlayer.tsx";
-import {headEmote as emote} from "./headEmote.ts";
+import emote from "./emotes/emote.json";
+import type {Emote} from "@/emoteAnimation/animationJson";
 
 const scene = {
   background: new Color("#012833")
@@ -11,16 +12,16 @@ const scene = {
 function App() {
   return (
     <>
-      <Canvas style={{height: "100vh"}} camera={{position: [0, 26, 40], }}  scene={scene}>
+      <Canvas style={{height: "100vh"}} camera={{position: [0, 26, 40], }} scene={scene}>
         <ambientLight  />
         <axesHelper args={[30]} />
-        <OrbitControls target={[0,16,0]} enablePan={false}
+        <OrbitControls target={[0,16,0]} enablePan
                        minDistance={10} maxDistance={50}
                        maxPolarAngle={Math.PI / 1.65} makeDefault
         />
         <Grid args={[60, 60]} sectionColor={"#089a0c"} cellColor={"#fff"} />
 
-        <EmotePlayer emote={emote} />
+        <EmotePlayer emote={emote as Emote} />
       </Canvas>
     </>
   );
