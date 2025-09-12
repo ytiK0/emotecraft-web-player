18d5498dc026096a4f458462b80aa07d42fe3ea9
ytiK0 <eeeee@eee.eee>
2025-09-12 18:46:40 +0300

    Adjust torso position to zeros and correct leg orientations.

--- src/lib/defaults.ts
+++ src/lib/defaults.ts
@@ -4,47 +4,46 @@ import type {Pose} from "@/player";
 export const defaultPose: Pose = {
   torso: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(0, 12, 0),
-    debug: true,
+    position: new Vector3(0, 0, 0),
   },
   torso_bend: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(0, 6, 0)
+    position: new Vector3(0, 6, 0),
   },
   head: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(0, 6, 0)
+    position: new Vector3(0, 6, 0),
   },
   leftArm: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(5, 4, 0)
+    position: new Vector3(5, 4, 0),
   },
   leftArm_bend: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(1, -4, 0)
+    position: new Vector3(1, -4, 0),
   },
   rightArm: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(-5, 4, 0)
+    position: new Vector3(-5, 4, 0),
   },
   rightArm_bend: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(-1, -4, 0)
+    position: new Vector3(-1, -4, 0),
   },
   leftLeg: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(-2, 0, 0)
+    position: new Vector3(2, 0, 0),
   },
   leftLeg_bend: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(0, -6, 0)
+    position: new Vector3(0, -6, 0),
   },
   rightLeg: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(2, 0, 0)
+    position: new Vector3(-2, 0, 0),
   },
   rightLeg_bend: {
     rotation: new Euler(0, 0, 0),
-    position: new Vector3(0, -6, 0)
+    position: new Vector3(0, -6, 0),
   }
 };
