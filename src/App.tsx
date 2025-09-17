364411dc95f6de4806c6f8f406498334d83f57e3
ytiK0 <eeeee@eee.eee>
2025-09-17 13:22:23 +0300

    Apply last updates in demo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {Canvas} from "@react-three/fiber";
 import {Grid, OrbitControls} from "@react-three/drei";
 import {Color} from "three";
+import type {Emote} from "@/emoteAnimation/animationJson";
 import EmotePlayer from "@/EmotePlayer.tsx";
 import emote from "./emotes/emote.json";
-import type {Emote} from "@/emoteAnimation/animationJson";
+
 
 const scene = {
   background: new Color("#012833")
@@ -13,15 +14,15 @@ function App() {
   return (
     <>
       <Canvas style={{height: "100vh"}} camera={{position: [0, 26, 40], }} scene={scene}>
-        <ambientLight  />
+        <ambientLight intensity={1.5} />
         <axesHelper args={[30]} />
         <OrbitControls target={[0,16,0]} enablePan
-                       minDistance={10} maxDistance={50}
-                       maxPolarAngle={Math.PI / 1.65} makeDefault
+                       maxDistance={50} makeDefault
         />
         <Grid args={[60, 60]} sectionColor={"#089a0c"} cellColor={"#fff"} />
 
         <EmotePlayer emote={emote as Emote} />
+        {/*<BasePlayerModel />*/}
       </Canvas>
     </>
   );
