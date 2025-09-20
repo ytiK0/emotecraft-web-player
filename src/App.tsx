6413ae0af82d9406077724e87a5cdb014cbed19f
ytiK0 <eeeee@eee.eee>
2025-09-20 20:18:04 +0300

    Clean up demo/dev App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import {Canvas} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 import {Color} from "three";
-import type {Emote} from "@/emoteAnimation/types/animationJson";
+import emote from "./emotes/SPE_Boogie down.json";
 import EmotePlayer from "@/EmotePlayer.tsx";
-import emote from "./emotes/SPE_Witch Pose.json";
-
+import type {Emote} from "@/emoteAnimation/types/animationJson";
 
 const scene = {
   background: new Color("#012833")
@@ -13,16 +12,15 @@ const scene = {
 function App() {
   return (
     <>
-      <Canvas style={{height: "100vh"}} camera={{position: [0, 26, 40], }} scene={scene}>
-        <ambientLight intensity={1.5} />
-        <axesHelper args={[30]} />
-        <OrbitControls target={[0,16,0]} enablePan
+      <Canvas style={{height: "100vh"}} camera={{position: [0, 26, 40],}} scene={scene}>
+        <ambientLight intensity={1.5}/>
+        <axesHelper args={[30]}/>
+        <OrbitControls target={[0, 16, 0]} enablePan
                        maxDistance={50} makeDefault
         />
-        {/*<Grid  args={[60, 60]} sectionColor={"#089a0c"} cellColor={"#fff"} />*/}
 
-        <EmotePlayer emote={emote as Emote} />
-        {/*<BasePlayerModel />*/}
+
+        <EmotePlayer emote={emote as Emote}/>
       </Canvas>
     </>
   );
