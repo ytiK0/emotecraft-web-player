83eff64a2798aa429b959b2b04289dc148a516e0
ytiK0 <eeeee@eee.eee>
2025-09-28 21:20:50 +0300

    Build config created, dependencies changed to peerDeps

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,33 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+
   plugins: [react()],
   resolve: {
     alias: {
       '@': resolve(import.meta.dirname, 'src/lib'),
     },
   },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/lib/index.ts"),
+      fileName: (format) => `emotecraft-web-player.${format}.js`,
+      formats: ["cjs", "es"]
+    },
+    minify: true,
+    rollupOptions: {
+      external: ["react", "react-dom", "three", "@react-three/fiber", "@react-three/drei"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          three: "THREE",
+          "@react-three/fiber": "ReactThreeFiber",
+          "@react-three/drei": "ReactThreeDrei"
+        },
+      },
+      treeshake: true,
+
+    }
+  }
 });
