1288339c5a81fe65a6dbc615a26ffa9335b3410a
ytiK0 <tiprson@gmail.com>
2025-10-02 18:57:24 +0300

    Refactor BendableArm components to support slim arm configurations.

--- src/lib/bodyParts/bended/BendableArms.tsx
+++ src/lib/bodyParts/bended/BendableArms.tsx
@@ -1,30 +1,52 @@
-import {Vector2, Vector3, type Vector3Tuple} from "three";
+import {Vector3, type Vector3Tuple} from "three";
 import {forwardRef} from "react";
-import type {BodyPartProps, TextureConfig} from "@/bodyParts/types/bodyPart";
+import type {BodyPartProps} from "@/bodyParts/types/bodyPart";
 import {BendableBodyPartBase} from "@/bodyParts/bended/BendableBodyPartBase.tsx";
 import type {DisposableBodyPartRepresentation} from "@/types/playerModel";
+import {buildTextureConfig} from "@/utils/buildTextureConfig.ts";
+
+type  ArmsProps = BodyPartProps & {
+  isSlim?: boolean
+}
 
 const armSize = [4, 12, 4] as Vector3Tuple;
-const armGeometrySize = armSize.map((e) => e - 0.1) as Vector3Tuple;
+const slimArmSize = [3, 12, 4] as Vector3Tuple;
+
+const armGeometrySize = {
+  default: armSize.map((e) => e - 0.01) as Vector3Tuple,
+  slim: slimArmSize.map((e) => e - 0.01) as Vector3Tuple
+} ;
 
 const leftArmPivotPointShift = new Vector3(1, -4, 0);
-const leftArmTextureConfig: TextureConfig = {
-  textureStart: new Vector2(32, 64 - 16),
-  textureSizes: new Vector3(...armSize)
+const leftArmTextureConfig = {
+  default: buildTextureConfig(32, 64 - 16, armSize),
+  slim: buildTextureConfig(32, 64 - 16, slimArmSize)
 };
-const leftArmOverlayTextureConfig: TextureConfig = {
-  textureStart: new Vector2(3 * 16, 64 - 16),
-  textureSizes: new Vector3(...armSize)
+const leftArmOverlayTextureConfig = {
+  default: buildTextureConfig(32 + 16, 64 - 16, armSize),
+  slim: buildTextureConfig(32 + 16, 64 - 16, slimArmSize)
 };
 
-export const BendableLeftArm = forwardRef<DisposableBodyPartRepresentation, BodyPartProps>(({children, name, debug, position}: BodyPartProps, ref) => {
+export const BendableLeftArm = forwardRef<DisposableBodyPartRepresentation, ArmsProps>(({
+  children,
+  name,
+  debug,
+  position,
+  isSlim
+}, ref) => {
+  const version = isSlim ? "slim" : "default";
+
+  const textureConfig = leftArmTextureConfig[version];
+  const overlayTextureConfig = leftArmOverlayTextureConfig[version];
+  const partSize = armGeometrySize[version];
+
   return (
     <BendableBodyPartBase
       ref={ref}
       pivotShift={leftArmPivotPointShift}
-      partSize={armGeometrySize}
-      textureConfig={leftArmTextureConfig}
-      overlayTextureConfig={leftArmOverlayTextureConfig}
+      partSize={partSize}
+      textureConfig={textureConfig}
+      overlayTextureConfig={overlayTextureConfig}
       position={position}
       name={name}
       debug={debug}
@@ -35,22 +57,35 @@ export const BendableLeftArm = forwardRef<DisposableBodyPartRepresentation, Body
 });
 
 const rightArmPivotPointShift = new Vector3(-1, -4, 0);
-const rightArmTextureConfig: TextureConfig = {
-  textureStart: new Vector2(32 + 8, 16),
-  textureSizes: new Vector3(...armSize)
+const rightArmTextureConfig = {
+  default: buildTextureConfig(32 + 8, 16, armSize),
+  slim: buildTextureConfig(32 + 8, 16, slimArmSize)
 };
-const rightArmOverlayTextureConfig: TextureConfig = {
-  textureStart: new Vector2(32 + 8, 32),
-  textureSizes: new Vector3(...armSize)
+const rightArmOverlayTextureConfig =  {
+  default: buildTextureConfig(32 + 8, 32, armSize),
+  slim: buildTextureConfig(32 + 8, 32, slimArmSize)
 };
-export const BendableRightArm = forwardRef<DisposableBodyPartRepresentation, BodyPartProps>(({children, name, debug, position}: BodyPartProps, ref) => {
+
+export const BendableRightArm = forwardRef<DisposableBodyPartRepresentation, ArmsProps>(({
+  children,
+  name,
+  debug,
+  position,
+  isSlim
+}, ref) => {
+  const version = isSlim ? "slim" : "default";
+
+  const textureConfig = rightArmTextureConfig[version];
+  const overlayTextureConfig = rightArmOverlayTextureConfig[version];
+  const partSize = armGeometrySize[version];
+
   return (
     <BendableBodyPartBase
       ref={ref}
       pivotShift={rightArmPivotPointShift}
-      partSize={armGeometrySize}
-      textureConfig={rightArmTextureConfig}
-      overlayTextureConfig={rightArmOverlayTextureConfig}
+      partSize={partSize}
+      textureConfig={textureConfig}
+      overlayTextureConfig={overlayTextureConfig}
       position={position}
       name={name}
       debug={debug}
