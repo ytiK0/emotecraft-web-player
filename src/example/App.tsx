f386c6a7c8c50b50688b28ecff06a7021df3e519
ytiK0 <tiprson@gmail.com>
2025-10-02 19:04:54 +0300

    Refactor App component to handle skin source and slim model states.

--- src/example/App.tsx
+++ src/example/App.tsx
@@ -1,9 +1,7 @@
 import {EmotePlayerScene} from "@/EmotePlayerScene.tsx";
 import EmotePlayer from "@/EmotePlayer.tsx";
-import {Vector3} from "three";
 import {type ChangeEvent, useCallback, useEffect, useRef, useState} from "react";
 import type {EmotePlayerAPI} from "@/types/playerModel";
-import type {Emote} from "../lib/emoteAnimation/types/animationJson";
 
 
 function App() {
@@ -26,7 +24,9 @@ function App() {
     return () => window.removeEventListener("keydown", onKeyDown);
   }, []);
 
-  const [emote, setEmote] = useState<Emote>();
+  const [emote, setEmote] = useState<unknown>();
+  const [skinSrc, setSkinSrc] = useState("https://mc-heads.net/skin/MrEka_");
+  const [isSlim, setIsSlim] = useState(false);
 
   const onEmoteChange = useCallback((ev: ChangeEvent<HTMLInputElement>) => {
     const target = ev.target;
@@ -40,14 +40,37 @@ function App() {
     }
   }, []);
 
+  const onSkinSrcChange = useCallback((ev: ChangeEvent<HTMLInputElement>) => {
+    const target = ev.target;
+
+    setSkinSrc(target.value);
+  }, []);
+
+  const onSlimChange = useCallback((ev: ChangeEvent<HTMLInputElement>) => {
+    const target = ev.target;
+
+    setIsSlim(target.checked);
+  }, []);
+
   return (
     <div style={{ display: "flex" }}>
        <EmotePlayerScene style={{ width: "70%", height: "100vh", margin: 0}} defaultScene >
-         <EmotePlayer ref={playerApiRef} emote={emote} playerModelType={"bend"} playerModelPosition={new Vector3(0,0,0)}  />
+         <EmotePlayer
+           ref={playerApiRef}
+           skinSrc={skinSrc}
+           emote={emote}
+           isSlimModel={isSlim}
+         />
        </EmotePlayerScene>
-      <div>
+      <div style={{ display: "flex", flexDirection: "column", gap: 8}}>
         <label htmlFor="emote-file-input">Эмоция</label>
         <input onChange={onEmoteChange} id={"emote-file-input"} type="file" accept={"application/json"} />
+        <label htmlFor="skinsrc">Ссылка на источник скина</label>
+        <input id={"skinsrc"} type="url" value={skinSrc} onChange={onSkinSrcChange} />
+        <div>
+          <label htmlFor="isslim">SlimModel</label>
+          <input type={"checkbox"} onChange={onSlimChange} checked={isSlim}/>
+        </div>
       </div>
     </div>
   );
