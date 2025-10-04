b1a089f8e6abc588013221429f65d52a7ba05fc5
ytiK0 <tiprson@gmail.com>
2025-10-04 12:58:20 +0300

    Refactor TypeScript configuration by adding exclusion for node_modules, integrating vite-plugin-dts, and creating a new build configuration for improved type declaration handling.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
 
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      insertTypesEntry: true,
+      tsconfigPath: "./tsconfig.app.json"
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(import.meta.dirname, 'src/lib'),
@@ -15,22 +23,24 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "./src/lib/index.ts"),
       fileName: (format) => `emotecraft-web-player.${format}.js`,
-      formats: ["cjs", "es"]
+      formats: ["es", "cjs"]
     },
     minify: true,
     rollupOptions: {
-      external: ["react", "react-dom", "three", "@react-three/fiber", "@react-three/drei"],
+      external: [/^react($|\/)/, "react-dom", "three", "@react-three/fiber", "@react-three/drei", "easing-functions"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "react/jsx-runtime": "ReactJSXRuntime",
+          "react/jsx-dev-runtime": "ReactJSXDevRuntime",
           three: "THREE",
           "@react-three/fiber": "ReactThreeFiber",
-          "@react-three/drei": "ReactThreeDrei"
+          "@react-three/drei": "ReactThreeDrei",
+          "easing-functions": "EasingFunctions"
         },
       },
       treeshake: true,
-
     }
   }
 });
